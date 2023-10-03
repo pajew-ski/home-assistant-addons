9514f099176359dd31e46e94e995a43f5ddd3a81
Michael Pajewski <80986335+pajew-ski@users.noreply.github.com>
2023-10-03 14:33:53 +0200

    run

--- ai/run.sh
+++ ai/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
 #echo "All done!" > /share/example_addon_output_ready.txt
-# python3 -m http.server 8099
 
-python3 pip install chainlit
+python3 -m http.server 8099
 
-python3 chainlit run app.py --port 8099
+#python3 chainlit run app.py --port 8099
+
