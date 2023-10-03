9b60f138ec80d005995a19cb10c64ff7d2a7099c
Michael Pajewski <80986335+pajew-ski@users.noreply.github.com>
2023-10-03 14:12:23 +0200

    run

--- ai/run.sh
+++ ai/run.sh
@@ -3,6 +3,6 @@
 #echo "All done!" > /share/example_addon_output_ready.txt
 # python3 -m http.server 8099
 
-pip install chainlit
+python3 pip install chainlit
 
 python3 chainlit run app.py --port 8099
